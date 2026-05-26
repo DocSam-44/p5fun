53cffcd2e4135e4b5b848a46bb13d5b712dace6a
DocSam-44 <sammeemayer4@gmail.com>
2026-05-26 15:05:22 -0400

    added 2 circles and a triangle in class

--- sketch.js
+++ sketch.js
@@ -1,7 +1,65 @@
 // the setup & draw functions are 'reserved functions' in p5js, meaning p5js uses it internally. notice there are no function calls in this script, yet the code runs in the page! (p5 calls these automatically)
+/*
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
 }
+let circles = []
+
 function draw() {
-  background("aqua");
+  background("royalblue");
+  circle(200,200,100);
+    fill('green');
+// x-position, y-position, size
+
+
+if(mouseIsPressed){
+circle(200,200,30);
+    fill('red');
+
+
+    circles.push([mouseX, mouseY])
+    console.log(circles)
+}
+
+if(keyIsPressed){
+triangle(100, 100, 150, 200, 250, 100);
+fill(yellow)
+
+}
+
+
+
+}
+
+*/
+
+function setup() {
+  createCanvas(600, 600);
+}
+
+let circles = [];
+
+function draw() {
+  background("royalblue");
+
+  // Big green circle
+  fill("red");
+  circle(200, 200, 100);
+
+  // Small red circle while mouse pressed
+  if (mouseIsPressed) {
+    fill("green");
+    circle(200, 200, 30);
+
+    circles.push([mouseX, mouseY]);
+    console.log(circles);
+  }
+
+  for(i=0; i< circles.length; i++)
+
+  // Triangle only while key pressed
+  if (keyIsPressed) {
+    fill("yellow");
+    triangle(100, 100, 150, 200, 250, 100);
+  }
 }
