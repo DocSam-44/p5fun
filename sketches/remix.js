f0e7ed367eb59628d585804353d6611cf4e8f91f
DocSam-44 <sammeemayer4@gmail.com>
2026-05-28 15:45:32 -0400

    remix is coming along with two smiles now

--- sketches/remix.js
+++ sketches/remix.js
@@ -3,12 +3,167 @@
 // This is where sketch1 and sketch2 come together
 // into something new
 // =============================================
+let img1;
+let img2;
+let particleSystem;
+
+let stations = [
+  { x: 200, y: 200 },
+  { x: 350, y: 200 }
+];
+
+function preload() {
+  img1 = loadImage('dev/anime_-smile_with_-sharp_-teeth-jpg-p63eqau3yjbxnj6r.jpg');
+  img2 = loadImage('dev/st,small,507x507-pad,600x600,f8f8f8.u2.jpg');
+}
 
 function setup() {
-  createCanvas(800, 500);
+  createCanvas(600, 600);
+  imageMode(CENTER);
+  particleSystem = new ParticleSystem();
 }
 
 function draw() {
-  background(220);
-  // your combined code here
+  background(0);
+
+  // =======================
+  // SCALE CONTROL (CHANGE THIS)
+  // =======================
+  let imgScale = 1.6;
+
+  // =======================
+  // MOVING LINE
+  // =======================
+  let lineY = frameCount % height;
+
+  // =======================
+  // IMAGE SIZE
+  // =======================
+  let squareSize = map(
+    noise(frameCount * 0.02),
+    0,
+    1,
+    120,
+    220
+  );
+
+  let scaledSize = squareSize * imgScale;
+
+  let squareX = width / 2;
+  let squareY = height / 1.5;
+
+  // =======================
+  // BASE IMAGE (img1)
+  // =======================
+  image(img1, squareX, squareY, scaledSize, scaledSize);
+
+  // =======================
+  // REVEAL IMAGE (img2)
+  // =======================
+  let topEdge = squareY - scaledSize / 2;
+  let revealHeight = constrain(lineY - topEdge, 0, scaledSize);
+
+  if (revealHeight > 0) {
+    copy(
+      img2,
+      0, 0,
+      img2.width,
+      img2.height * (revealHeight / scaledSize),
+      squareX - scaledSize / 2,
+      squareY - scaledSize / 2,
+      scaledSize,
+      revealHeight
+    );
+  }
+
+  // =======================
+  // STATIONS
+  // =======================
+  fill("red");
+  noStroke();
+  for (let s of stations) {
+    circle(s.x, s.y, 100);
+  }
+
+  // =======================
+  // BLACK CIRCLES (ON PRESS)
+  // =======================
+  if (mouseIsPressed) {
+    fill("black");
+    noStroke();
+    for (let s of stations) {
+      circle(s.x, s.y, 30);
+    }
+
+    // particles
+    for (let s of stations) {
+      particleSystem.origin = createVector(s.x, s.y);
+
+      for (let i = 0; i < 3; i++) {
+        particleSystem.addParticle();
+      }
+    }
+  }
+
+  particleSystem.run();
+
+  // =======================
+  // WHITE LINE
+  // =======================
+  stroke(255);
+  strokeWeight(5);
+  line(0, lineY, width, lineY);
+}
+
+
+// =======================
+// PARTICLE SYSTEM
+// =======================
+
+class ParticleSystem {
+  constructor() {
+    this.particles = [];
+    this.origin = createVector(0, 0);
+  }
+
+  addParticle() {
+    this.particles.push(new Particle(this.origin.copy()));
+  }
+
+  run() {
+    for (let i = this.particles.length - 1; i >= 0; i--) {
+      let p = this.particles[i];
+      p.update();
+      p.display();
+
+      if (p.isDead()) {
+        this.particles.splice(i, 1);
+      }
+    }
+  }
+}
+
+class Particle {
+  constructor(position) {
+    this.position = position.copy();
+    this.velocity = createVector(random(-1, 1), random(-3, -1));
+    this.acceleration = createVector(0, 0.02);
+    this.lifespan = 255;
+  }
+
+  update() {
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.lifespan -= 4;
+  }
+
+  display() {
+    noStroke();
+    fill(0, 255, 100, this.lifespan);
+    circle(this.position.x, this.position.y, 10);
+  }
+
+  isDead() {
+    return this.lifespan <= 0;
+  }
 }
--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -3,11 +3,202 @@
 // Paste your source sketch code here and start hacking
 // =============================================
 
+/*
+let img1;
+let img2;
+
+function preload() {
+  img1 = loadImage('img1.jpg');
+  img2 = loadImage('img2.jpg');
+}
+
 function setup() {
-  createCanvas(800, 500);
+  createCanvas(600, 400);
+  imageMode(CENTER);
 }
 
 function draw() {
-  background(220);
-  // your code here
+  background(0);
+
+  // moving line
+  let lineY = frameCount % height;
+
+  // animated square size
+  let squareSize = map(
+    noise(frameCount * 0.02),
+    0,
+    1,
+    120,
+    220
+  );
+
+  let squareX = width / 1.7;
+  let squareY = height / 1.5;
+
+  // square boundaries
+  let topEdge = squareY - squareSize / 2;
+
+  // ===== DRAW IMAGE 1 INSIDE SQUARE =====
+
+  image(img1, squareX, squareY, squareSize, squareSize);
+
+  // ===== REVEAL IMAGE 2 AS LINE PASSES =====
+
+  // how much of square has been crossed by line
+  let revealHeight = constrain(lineY - topEdge, 0, squareSize);
+
+  if (revealHeight > 0) {
+
+    copy(
+      img2,
+
+      // source image crop
+      0,
+      0,
+      img2.width,
+      map(revealHeight, 0, squareSize, 0, img2.height),
+
+      // destination on canvas
+      squareX - squareSize / 2,
+      squareY - squareSize / 2,
+      squareSize,
+      revealHeight
+    );
+  }
+
+  // ===== WHITE MOVING LINE =====
+
+  stroke(255);
+  strokeWeight(7);
+  line(0, lineY, width, lineY);
 }
+*/
+
+// let img1;
+// let img2;
+// let squareX = width / 6.7;
+// let squareY = height / 1.5;
+
+
+// function setup() {
+//   createCanvas(600, 400);
+//   imageMode(CENTER);
+// }
+
+// function draw() {
+//   background(0);
+
+//   let lineY = frameCount % height;
+
+//   let squareSize = map(
+//     noise(frameCount * 0.02),
+//     0,
+//     1,
+//     120,
+//     220
+//   );
+
+//   let topEdge = squareY - squareSize / 2;
+
+//   // draw base image
+//   image(img1, squareX, squareY, squareSize, squareSize);
+
+//   // reveal second image
+//   let revealHeight = constrain(lineY - topEdge, 0, squareSize);
+
+//   if (revealHeight > 0) {
+//     copy(
+//       img2,
+//       0,
+//       0,
+//       img2.width,
+//       img2.height * (revealHeight / squareSize),
+//       squareX - squareSize / 2,
+//       squareY - squareSize / 2,
+//       squareSize,
+//       revealHeight
+//     );
+//   }
+
+//   stroke(255);
+//   strokeWeight(7);
+//   line(0, lineY, width, lineY);
+// }
+
+// console.log(img1, img2);
+
+// ! commented out the above code to try and locate why the images wont move to the right
+// let img1;
+// let img2;
+
+// function preload() {
+//   img1 = loadImage("img1.jpg");
+//   img2 = loadImage("img2.jpg");
+// }
+
+// function setup() {
+//   createCanvas(600, 400);
+//   imageMode(CENTER);
+// }
+
+/*
+function draw() {
+  background(0);
+
+  // moving line
+  let lineY = frameCount % height;
+
+  // position (TRY mouseX if you want interaction)
+  let squareX = width/10;   // LEFT SIDE
+  let squareY = height * 0.6;
+
+  // size animation
+  let squareSize = map(
+    noise(frameCount * 0.02),
+    0,
+    1,
+    180,
+    320
+  );
+
+  let topEdge = squareY - squareSize / 2;
+
+  // ===== BASE IMAGE (img1) =====
+  image(img1, squareX, squareY, squareSize, squareSize);
+
+  // ===== REVEAL IMAGE (img2) =====
+  let revealHeight = constrain(lineY - topEdge, 0, squareSize);
+
+  if (revealHeight > 0) {
+    copy(
+      img2,
+      0,
+      0,
+      img2.width,
+      img2.height * (revealHeight / squareSize),
+      squareX - squareSize / 2,
+      squareY - squareSize / 2,
+      squareSize,
+      revealHeight
+    );
+  }
+
+  // ===== WHITE LINE =====
+  stroke(255);
+  strokeWeight(5);
+  line(0, lineY, width, lineY);
+}
+*/
+
+// function draw() {
+//   background(255,0,0);
+
+//   let squareX = mouseX;
+//   let squareY = height / 2;
+
+//   fill(255);
+//   circle(squareX, squareY, 50);
+// }
+
+
+// just realized i was editing the wrong file so the below code is new and what i tried to do here is complete in the remix
